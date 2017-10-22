2b6360670238ef30f03f155839809c2801c63ab1
Lucas Waldburger <30703175+lucaswaldburger@users.noreply.github.com>
2017-10-21 21:38:16 -0700

    keeping it simple

--- autocrispr.py
+++ autocrispr.py
@@ -1,5 +1,6 @@
-
-import curses
+import sys
+import os
+import time
 from LW_DNA_tools import *
 
 '''
@@ -21,7 +22,6 @@ class Design_CRISPR_Oligos:
         # self.genelocus = genelocus
         self.protospacer = protospacer
         self.genomicseq = genomicseq
-        #
         # if self.genelocus in loci:
         #     loci[self.genelocus] += 1
         #     num = loci[self.genelocus]
@@ -32,161 +32,46 @@ class Design_CRISPR_Oligos:
 
     def run(self):
         # 5' --> 3' forward gRNA oligo sequence
-        fw_gRNA = 'GACTTT' + self.protospacer
+        fwd_gRNA = 'GACTTT' + self.protospacer
 
         # 5' --> 3' reverse gRNA oligo sequence
         rev_gRNA = 'AAAC' + reverse_complement(self.protospacer) + 'AA'
-        self.guides = [fw_gRNA, rev_gRNA]
 
         # 5' --> 3' forward repair DNA oligo sequence
-        fw_repairDNA = self.genomicseq[950:950 + 50] + self.genomicseq[
+        fwd_repairDNA = self.genomicseq[950:950 + 50] + self.genomicseq[
                                                        len(self.genomicseq) - 1000:len(self.genomicseq) - 1000 + 8]
         # 3' --> 5' reverse repair DNA oligo sequence
         rev_repairDNA = self.genomicseq[1000 - 8:1000] + self.genomicseq[
                                                          len(self.genomicseq) - 1000:len(self.genomicseq) - 1000 + 50]
-        self.repairs = [fw_repairDNA, reverse_complement(rev_repairDNA)]
 
-        return [self.guides, self.repairs]
-
-def my_raw_input(stdscr, r, c, prompt_string):
-    curses.echo()
-    stdscr.addstr(r, c, prompt_string)
-    stdscr.refresh()
-    input = stdscr.getstr(r + 1, c, 20)
-    return input  #       ^^^^  reading input at next line
-
-def draw_menu(stdscr):
-    k = 0
-    cursor_x = 0
-    cursor_y = 0
-
-    # Clear and refresh the screen for a blank canvas
-    stdscr.clear()
-    stdscr.refresh()
-
-    # Start colors in curses
-    curses.start_color()
-    curses.init_pair(1, curses.COLOR_CYAN, curses.COLOR_BLACK)
-    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
-    curses.init_pair(3, curses.COLOR_BLACK, curses.COLOR_WHITE)
-
-    # Loop where k is the last character pressed
-    while (k != ord('q')):
-
-        # Initialization
-        stdscr.clear()
-        height, width = stdscr.getmaxyx()
-
-        # if k == curses.KEY_DOWN:
-        #     cursor_y = cursor_y + 1
-        # elif k == curses.KEY_UP:
-        #     cursor_y = cursor_y - 1
-        # elif k == curses.KEY_RIGHT:
-        #     cursor_x = cursor_x + 1
-        # elif k == curses.KEY_LEFT:
-        #     cursor_x = cursor_x - 1
-        #
-        # cursor_x = max(0, cursor_x)
-        # cursor_x = min(width-1, cursor_x)
-        #
-        # cursor_y = max(0, cursor_y)
-        # cursor_y = min(height-1, cursor_y)
-
-        # Declaration of strings
-        title = "Go Go CRISPR!"[:width-1]
-        subtitle = "Written by Lucas Waldburger"[:width-1]
-        # keystr = "Last key pressed: {}".format(k)[:width-1]
-        statusbarstr = "Press 'q' to exit | STATUS BAR | Pos: {}, {}".format(cursor_x, cursor_y)
-        # if k == 0:
-        #     keystr = "No key press detected..."[:width-1]
-
-        # Centering calculations
-        start_x_title = int((width // 2) - (len(title) // 2) - len(title) % 2)
-        start_x_subtitle = int((width // 2) - (len(subtitle) // 2) - len(subtitle) % 2)
-        # start_x_keystr = int((width // 2) - (len(keystr) // 2) - len(keystr) % 2)
-        start_y = int((height // 2) - int(height // 2.1))
-
-        # Rendering some text
-        # whstr = "Width: {}, Height: {}".format(width, height)
-        # stdscr.addstr(0, 0, whstr, curses.color_pair(1))
-
-        # Render status bar
-        stdscr.attron(curses.color_pair(3))
-        stdscr.addstr(height-1, 0, statusbarstr)
-        stdscr.addstr(height-1, len(statusbarstr), " " * (width - len(statusbarstr) - 1))
-        stdscr.attroff(curses.color_pair(3))
-
-        # Turning on attributes for title
-        stdscr.attron(curses.color_pair(1))
-        stdscr.attron(curses.A_BOLD)
-
-        # Rendering title
-        stdscr.addstr(start_y, start_x_title, title)
-
-        # Turning off attributes for title
-        stdscr.attroff(curses.color_pair(1))
-        stdscr.attroff(curses.A_BOLD)
-
-        # Print rest of text
-        stdscr.addstr(start_y + 1, start_x_subtitle, subtitle)
-        # stdscr.addstr(start_y + 3, (width // 2) - 2, '-' * 4)
-        # stdscr.addstr(start_y + 5, start_x_keystr, keystr)
-        # stdscr.move(cursor_y, cursor_x)
-
-
-
-        # #user intials
-        # user_init = my_raw_input(stdscr, 5, int(width // 10), "User initials: ").lower()
-        # if user_init != "" and len(user_init) > 1 and len(user_init) < 3 :
-        #     stdscr.addstr(5,int(width-30) , "User Added!")
-        # else:
-        #     stdscr.addstr(5,int(width-30), " Invalid input")
-
-        # #current oligo
-        # start_oligo = my_raw_input(stdscr, 7, int(width // 10), "Current oligo: ").lower()
-        # if user_init != "":
-        #     stdscr.addstr(7,int(width-30), "Position Added!")
-        # else:
-        #     stdscr.addstr(7,int(width-30), " Invalid input")
-        #
-        # #locus name
-        # locus_name = my_raw_input(stdscr, 9, int(width // 10), "Locus name: ").lower()
-        # if locus_name != "":
-        #     stdscr.addstr(9,int(width-30), "Name Added!")
-        # else:
-        #     stdscr.addstr(9,int(width-30), " Invalid input")
-
-        #gRNA seq
-        grna_in = my_raw_input(stdscr, 5, int(width // 10), "Protospacer: ").lower()
-        if grna_in != "" and len(grna_in) == 20:
-            stdscr.addstr(5,int(width-30), "Protospacer Added!")
-        else:
-            stdscr.addstr(5,int(width-30), "Invalid input")
-
-        #genomic seq
-        seq_in = my_raw_input(stdscr, 7, int(width // 10), "Genomic sequence (+/- 1 kb): ").lower()
-        if seq_in != "" and len(seq_in) > 2000:
-            stdscr.addstr(7,int(width-30), "Sequence Added!")
-        else:
-            stdscr.addstr(7,int(width-30), "Invalid input")
-
-        oligos = Design_CRISPR_Oligos(grna_in,seq_in).run()
-
-
-        stdscr.addstr(10,int(width // 10),"OUTPUT")
-        stdscr.addstr(12, int(width // 10), "forward gRNA \t reverse gRNA \t forward repair DNA \t reverse repair DNA")
-        stdscr.addstr(13,int(width//2),oligos[0][0] + "\t" + oligos[0][1] + "\t" + oligos[1][0] + "\t" + oligos[1][1]  )
-
-        # stdscr.addstr(25,30, oligos[0][0] + '\t' + oligos[0][1])
-
-        # Refresh the screen
-        stdscr.refresh()
-
-        # Wait for next input
-        k = stdscr.getch()
-
+        return [fwd_gRNA,rev_gRNA,fwd_repairDNA,reverse_complement(rev_repairDNA)]
+def spinning_cursor():
+    while True:
+        for cursor in '|/-\\':
+            yield cursor
 def main():
-    curses.wrapper(draw_menu)
-
-if __name__ == "__main__":
-    main()
+    print("GO GO CRISPR!")
+    protospacer_in = ""
+    sequence_in = ""
+    while(protospacer_in == "" or sequence_in == ""):
+        protospacer_in = raw_input("Protospacer Sequence: ")
+        if protospacer_in == "" or len(protospacer_in) != 20:
+            print("Invalid input")
+            protospacer_in = ""
+        sequence_in = raw_input("Genomic Sequence (+/- 1kb): ")
+        if sequence_in == "" or len(sequence_in) < 2000:
+            print("Invalid input")
+            sequence_in = ""
+            time.sleep(2)
+    spinner = spinning_cursor()
+    for _ in range(50):
+        sys.stdout.write(spinner.next())
+        sys.stdout.flush()
+        time.sleep(0.1)
+        sys.stdout.write('\b')
+    print("OUTPUT")
+    print("fwd gRNA \t rev gRNA \t fwd repair DNA \t rev repair DNA")
+    oligos = Design_CRISPR_Oligos(protospacer_in,sequence_in).run()
+    for i in oligos:
+        print(i)
+main()
